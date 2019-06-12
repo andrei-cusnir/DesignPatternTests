5e356a97e073cef3cb36002560502d4f8fdd215b
Andrei Cusnir <a.cusnir@developmentaid.org>
2019-06-12 15:18:09 +0300

    thats something2

--- SimUDuck/src/DuckBehaviors/FlyRocketPowered.java
+++ SimUDuck/src/DuckBehaviors/FlyRocketPowered.java
@@ -1,8 +1,9 @@
 package DuckBehaviors;
+
 public class FlyRocketPowered implements FlyBehavior {
-    @Override
+
     public void fly() {
         System.out.println("I'm flying with a rocket");
     }
 }
-}
+
